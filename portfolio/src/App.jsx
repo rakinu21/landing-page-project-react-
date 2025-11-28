8e72c6f8f87e7cb4c11bdc8abb9ae3d1a64be7b4
rakinu21 <ralphkinethugdamin@gmail.com>
2025-11-28 10:32:38 +0800

    creat 2nd project portfolio

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+     <section className="app-container">
+
+      <main>
+
+        hello world
+      </main>
+     </section>
+  )
+}
+
+export default App
