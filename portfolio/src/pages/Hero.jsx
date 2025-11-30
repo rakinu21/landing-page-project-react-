9528a0d1cf88fef8aca82d624ab912ca5d7b3b49
rakinu21 <ralphkinethugdamin@gmail.com>
2025-11-30 10:41:25 +0800

    portfolio is done

--- portfolio/src/pages/Hero.jsx
+++ portfolio/src/pages/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import heroImage from '../assets/images/heroImage.png'
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import '../styles/Hero.css'
 export const Hero = () => {
   return (
@@ -7,7 +8,7 @@ export const Hero = () => {
 
         <div className="text-content">
 
-            <h1>EXPLORE MY <br/> PORTFOLIO</h1>
+            <h1> <span>EXPLORE </span> MY <br/>POR<span>TFOLIO</span></h1>
         </div>
 
        
@@ -31,9 +32,39 @@ export const Hero = () => {
 
           <p>I am Passionate About <br />creating websites <br /> that stand out <br />from the crown</p>
 
-     
+           
        </div>
+        
 
+        <div className="button-container">
+             
+              <ul className="social-list">
+        <li>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <FaFacebookF />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <FaTwitter />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <FaLinkedinIn />
+          </a>
+        </li>
+      </ul>
+             <button className='download-btn'>download cv</button>
+           </div>
          
     </section>
   )
